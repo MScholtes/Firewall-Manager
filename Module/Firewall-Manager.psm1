5dbfc350705ddd709580bc0be1d634a308e8ed1c
MScholtes <markusscholtes@hotmail.com>
2020-02-17 10:53:36 +0100

    Initial commit

---
+++ Module/Firewall-Manager.psm1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+Firewall-Manager is a module to manage firewall rules.
+.NOTES
+Version: 1.0.2
+Date: 2020-02-17
+Author: Markus Scholtes
+#>
+
+# Load modules manually for security reasons
+. "$PSScriptRoot/Export-FirewallRules.ps1"
+. "$PSScriptRoot/Import-FirewallRules.ps1"
+. "$PSScriptRoot/Remove-FirewallRules.ps1"
+
+# Export functions
+Export-ModuleMember -Function @('Export-FirewallRules',	'Import-FirewallRules',	'Remove-FirewallRules')
